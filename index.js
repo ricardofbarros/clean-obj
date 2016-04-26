91470b59e5d899ed4255894cb82613c3710791f2
ricardofbarros <ricardofbarros@hotmail.com>
2016-04-26 19:14:55 +0100

    refactor main fn

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
-//Dependencies
-var isEmpty = require('./empty');
+var isEmpty = require('./lib/empty')
+var toStr = Object.prototype.toString.call
 
-var cleanObj = function myself(obj, strict) {
-  // temp var to evaluate 
-  var temp;
+module.exports = cleanObj
 
-  for(var k in obj) {
-    if(typeof(obj[k]) === 'object') {
-      // Check for dates
-      if(Object.prototype.toString.call(obj[k]) !== '[object Date]') {         
-        // Strict mode - deletes null & undefined
-        if(strict) {
-          temp = myself(obj[k], strict);
-          
-          if(temp === null) {
-            delete obj[k];
-          } else {
-            obj[k] = temp;
-          }
-        } 
-        // Non-Strict mode - deletes only undefined
-        else {
-          if(obj[k] !== null) {
-            temp = myself(obj[k], strict);
-            
-            if(temp === null) {
-              delete obj[k];
-            } else {
-              obj[k] = temp;
-            }
-          }                
-        }
-      }
-    } else {
-      // If value = undefined, delete obj[key]
-      if(typeof(obj[k]) === 'undefined') {
-        delete obj[k];
-      }
-    }
-  }   
+function cleanObj (obj, strict) {
+  for (var k in obj) {
+    cleanProperty(k, obj[k], obj)
+  }
 
   // Check if the object is empty
   // If it is return null to delete the object
-  if(!isEmpty(obj)) {
-    return obj;        
+  if (!isEmpty(obj)) {
+    return obj
   } else {
-    return null;
-  } 
-};
+    return null
+  }
 
-module.exports = cleanObj;
+  function cleanProperty (key, value, ref) {
+    // If value is evaluated as a falsy value
+    if (!value) {
+      if (strict || (!strict && value == null)) {
+        return delete ref[key]
+      }
+    }
+
+    var typeOfValue = typeof value
+
+    // If value is an object (excluding date objects)
+    if (typeOfValue === 'object' && toStr(value) !== '[object Date]') {
+      return cleanObj(ref[key], strict)
+    }
+  }
+}
