8a6e77fe15e596104541e6de441d653fdc4e81ce
ricardofbarros <ricardofbarros@hotmail.com>
2016-04-26 21:20:13 +0100

    refactor tests

--- test/non-strict.js
+++ test/non-strict.js
@@ -1,19 +1,24 @@
-var Code = require('code');
-var expect = Code.expect;
-var cleanObj = require('../index.js');
+var mocha = require('mocha')
+var expect = require('chai').expect
+var cleanObj = require('../index.js')
 
-describe('Clean an object (non-strict mode)', function() {
+var describe = mocha.describe
+var it = mocha.it
 
-  var undfz;
-  
-  // Wow, so much diversification in this object  
+describe('Clean an object (non-strict mode)', function () {
+  var undfz
+  var fn = function () {}
+
+  // Wow, so much diversification in this object
   var obj = {
     string: 'testing',
     number: 1337,
     float: 3.14159,
+    falsy: 0,
+    bool: false,
     date: new Date(),
-    fn: function(value) {
-      return 'testing - '+value;
+    fn: function (value) {
+      return 'testing - ' + value
     },
     nullz: null,
     undf: undfz,
@@ -21,106 +26,159 @@ describe('Clean an object (non-strict mode)', function() {
       string: 'testing',
       number: 1337,
       float: 3.14159,
+      falsy: 0,
+      bool: false,
       date: new Date(),
-      fn: function(value) {
-        return 'testing - '+value;
+      fn: function (value) {
+        return 'testing - ' + value
       },
       nullz: null,
-      undf: undfz    
-    }    
-  };
-  
+      undf: undfz,
+      childrenArr: ['str', 1, 0, false, true, fn, null, undfz, 1337]
+    },
+    childrenArr: ['str', 1, 0, false, true, fn, null, undfz, 1337]
+  }
+
   // Clean it! non-strictly ofc
-  obj = cleanObj(obj);
-  
-  it('Should keep the property string (non-strict mode)', function(done) {
-    expect(obj).to.include('string');    
-    expect(obj.string).to.equal('testing');
-    done();
-  });
+  cleanObj(obj)
 
-  it('Should keep the property number (non-strict mode)', function(done) {
-    expect(obj).to.include('number');    
-    expect(obj.number).to.equal(1337);
-    done();    
-  });  
+  it('should keep the property string', function (done) {
+    expect(obj).to.include.keys('string')
+    expect(obj.string).to.equal('testing')
+    done()
+  })
 
-  it('Should keep the property float (non-strict mode)', function(done) {
-    expect(obj).to.include('float');    
-    expect(obj.float).to.equal(3.14159);
-    done();        
-  }); 
+  it('should keep the property number', function (done) {
+    expect(obj).to.include.keys('number')
+    expect(obj.number).to.equal(1337)
+    done()
+  })
 
-  it('Should keep the property date (non-strict mode)', function(done) {
-    expect(obj).to.include('date');    
-    expect(obj.date).to.be.a.date();
-    done();      
-  });
-  
-  it('Should keep the property fn (non-strict mode)', function(done) {
-    expect(obj).to.include('fn');    
-    expect(obj.fn).to.be.a.function();
-    done();      
-  });
+  it('should keep the property float', function (done) {
+    expect(obj).to.include.keys('float')
+    expect(obj.float).to.equal(3.14159)
+    done()
+  })
 
-  it('Should not keep the property nullz (non-strict mode)', function(done) {
-    expect(obj).to.include('nullz');
-    expect(obj.nullz).to.be.a.null();
-    done();    
-  });  
-  
-  it('Should not keep the property undf (non-strict mode)', function(done) {
-    expect(obj).to.not.include('undf');
-    done();        
-  });
-  
-  describe('Now it should have cleaned recursively as well (strict-mode)', function() {
+  it('should keep the property falsy', function (done) {
+    expect(obj).to.include.keys('falsy')
+    expect(obj.falsy).to.equal(0)
+    done()
+  })
 
-    it('childrenObj should exist (non-strict mode)', function(done) {
-      expect(obj).to.include('childrenObj');
-      expect(obj.childrenObj).to.be.an.object();
-      done();              
-    });
-    
-    it('Should keep the property string (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('string');    
-      expect(obj.childrenObj.string).to.equal('testing');
-      done();        
-    });
+  it('should keep the property bool', function (done) {
+    expect(obj).to.include.keys('bool')
+    expect(obj.bool).to.equal(false)
+    done()
+  })
 
-    it('Should keep the property number (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('number');    
-      expect(obj.childrenObj.number).to.equal(1337);
-      done();    
-    });  
+  it('should keep the property date', function (done) {
+    expect(obj).to.include.keys('date')
+    expect(obj.date).to.be.a('date')
+    done()
+  })
 
-    it('Should keep the property float (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('float');    
-      expect(obj.childrenObj.float).to.equal(3.14159);
-      done();           
-    }); 
+  it('should keep the property fn', function (done) {
+    expect(obj).to.include.keys('fn')
+    expect(obj.fn).to.be.a('function')
+    done()
+  })
 
-    it('Should keep the property date (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('date');    
-      expect(obj.childrenObj.date).to.be.a.date();
-      done();    
-    });
+  it('should delete the property nullz', function (done) {
+    expect(obj).to.not.include.keys('nullz')
+    done()
+  })
 
-    it('Should keep the property fn (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('fn');    
-      expect(obj.childrenObj.fn).to.be.a.function();
-      done();            
-    });
+  it('should delete the property undf', function (done) {
+    expect(obj).to.not.include.keys('undf')
+    done()
+  })
 
-    it('Should not keep the property nullz (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('nullz');
-      expect(obj.childrenObj.nullz).to.be.a.null();      
-      done();
-    });  
+  it('should clean childrenArr from null and undf values', function (done) {
+    expect(obj).to.include.keys('childrenArr')
+    expect(obj.childrenArr).to.eql([
+      'str',
+      1,
+      0,
+      false,
+      true,
+      fn,
+      1337
+    ])
+    done()
+  })
 
-    it('Should not keep the property undf (non-strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.not.include('undf');
-      done();
-    });
-  });
-});
+  describe('It should have cleaned recursively', function () {
+    it('childrenObj should exist', function (done) {
+      expect(obj).to.include.keys('childrenObj')
+      expect(obj.childrenObj).to.be.an('object')
+      done()
+    })
+
+    it('should keep the property string', function (done) {
+      expect(obj.childrenObj).to.include.keys('string')
+      expect(obj.childrenObj.string).to.equal('testing')
+      done()
+    })
+
+    it('should keep the property number', function (done) {
+      expect(obj.childrenObj).to.include.keys('number')
+      expect(obj.childrenObj.number).to.equal(1337)
+      done()
+    })
+
+    it('should keep the property float', function (done) {
+      expect(obj.childrenObj).to.include.keys('float')
+      expect(obj.childrenObj.float).to.equal(3.14159)
+      done()
+    })
+
+    it('should keep the property falsy', function (done) {
+      expect(obj.childrenObj).to.include.keys('falsy')
+      expect(obj.childrenObj.falsy).to.equal(0)
+      done()
+    })
+
+    it('should keep the property bool', function (done) {
+      expect(obj.childrenObj).to.include.keys('bool')
+      expect(obj.childrenObj.bool).to.equal(false)
+      done()
+    })
+
+    it('should keep the property date', function (done) {
+      expect(obj.childrenObj).to.include.keys('date')
+      expect(obj.childrenObj.date).to.be.a('date')
+      done()
+    })
+
+    it('should keep the property fn', function (done) {
+      expect(obj.childrenObj).to.include.keys('fn')
+      expect(obj.childrenObj.fn).to.be.a('function')
+      done()
+    })
+
+    it('should delete the property nullz', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('nullz')
+      done()
+    })
+
+    it('should delete the property undf', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('undf')
+      done()
+    })
+
+    it('should clean childrenArr from null and undf values', function (done) {
+      expect(obj.childrenObj).to.include.keys('childrenArr')
+      expect(obj.childrenObj.childrenArr).to.eql([
+        'str',
+        1,
+        0,
+        false,
+        true,
+        fn,
+        1337
+      ])
+      done()
+    })
+  })
+})
--- test/strict.js
+++ test/strict.js
@@ -1,19 +1,24 @@
-var Code = require('code');
-var expect = Code.expect;
-var cleanObj = require('../index.js');
+var mocha = require('mocha')
+var expect = require('chai').expect
+var cleanObj = require('../index.js')
 
-describe('Clean an object (strict mode)', function() {
+var describe = mocha.describe
+var it = mocha.it
 
-  var undfz;
-  
-  // Wow, so much diversification in this object  
+describe('Clean an object (strict mode)', function () {
+  var undfz
+  var fn = function () {}
+
+  // Wow, so much diversification in this object
   var obj = {
     string: 'testing',
     number: 1337,
     float: 3.14159,
+    falsy: 0,
+    bool: false,
     date: new Date(),
-    fn: function(value) {
-      return 'testing - '+value;
+    fn: function (value) {
+      return 'testing - ' + value
     },
     nullz: null,
     undf: undfz,
@@ -21,104 +26,151 @@ describe('Clean an object (strict mode)', function() {
       string: 'testing',
       number: 1337,
       float: 3.14159,
+      falsy: 0,
+      bool: false,
       date: new Date(),
-      fn: function(value) {
-        return 'testing - '+value;
+      fn: function (value) {
+        return 'testing - ' + value
       },
       nullz: null,
-      undf: undfz    
-    }    
-  };
-  
-  // Clean it!
-  obj = cleanObj(obj, true);
-  
-  it('Should keep the property string (strict mode)', function(done) {
-    expect(obj).to.include('string');    
-    expect(obj.string).to.equal('testing');
-    done();
-  });
+      undf: undfz,
+      childrenArr: ['str', 1, 0, false, true, fn, null, undfz, 1337]
+    },
+    childrenArr: ['str', 1, 0, false, true, fn, null, undfz, 1337]
+  }
 
-  it('Should keep the property number (strict mode)', function(done) {
-    expect(obj).to.include('number');    
-    expect(obj.number).to.equal(1337);
-    done();    
-  });  
+  // Clean it! strict mode
+  obj = cleanObj(obj, true)
 
-  it('Should keep the property float (strict mode)', function(done) {
-    expect(obj).to.include('float');    
-    expect(obj.float).to.equal(3.14159);
-    done();        
-  }); 
+  it('should keep the property string', function (done) {
+    expect(obj).to.include.keys('string')
+    expect(obj.string).to.equal('testing')
+    done()
+  })
 
-  it('Should keep the property date (strict mode)', function(done) {
-    expect(obj).to.include('date');    
-    expect(obj.date).to.be.a.date();
-    done();      
-  });
-  
-  it('Should keep the property fn (strict mode)', function(done) {
-    expect(obj).to.include('fn');    
-    expect(obj.fn).to.be.a.function();
-    done();      
-  });
+  it('should keep the property number', function (done) {
+    expect(obj).to.include.keys('number')
+    expect(obj.number).to.equal(1337)
+    done()
+  })
 
-  it('Should not keep the property nullz (strict mode)', function(done) {
-    expect(obj).to.not.include('nullz');
-    done();    
-  });  
-  
-  it('Should not keep the property undf (strict mode)', function(done) {
-    expect(obj).to.not.include('undf');
-    done();        
-  });
-  
-  describe('Now it should have cleaned recursively as well (strict-mode)', function() {
+  it('should keep the property float', function (done) {
+    expect(obj).to.include.keys('float')
+    expect(obj.float).to.equal(3.14159)
+    done()
+  })
 
-    it('childrenObj should exist (strict mode)', function(done) {
-      expect(obj).to.include('childrenObj');
-      expect(obj.childrenObj).to.be.an.object();
-      done();              
-    });
-    
-    it('Should keep the property string (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('string');    
-      expect(obj.childrenObj.string).to.equal('testing');
-      done();        
-    });
+  it('should delete the property falsy', function (done) {
+    expect(obj).to.not.include.keys('falsy')
+    done()
+  })
 
-    it('Should keep the property number (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('number');    
-      expect(obj.childrenObj.number).to.equal(1337);
-      done();    
-    });  
+  it('should delete the property bool', function (done) {
+    expect(obj).to.not.include.keys('bool')
+    done()
+  })
 
-    it('Should keep the property float (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('float');    
-      expect(obj.childrenObj.float).to.equal(3.14159);
-      done();           
-    }); 
+  it('should keep the property date', function (done) {
+    expect(obj).to.include.keys('date')
+    expect(obj.date).to.be.a('date')
+    done()
+  })
 
-    it('Should keep the property date (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('date');    
-      expect(obj.childrenObj.date).to.be.a.date();
-      done();    
-    });
+  it('should keep the property fn', function (done) {
+    expect(obj).to.include.keys('fn')
+    expect(obj.fn).to.be.a('function')
+    done()
+  })
 
-    it('Should keep the property fn (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.include('fn');    
-      expect(obj.childrenObj.fn).to.be.a.function();
-      done();            
-    });
+  it('should delete the property nullz', function (done) {
+    expect(obj).to.not.include.keys('nullz')
+    done()
+  })
 
-    it('Should not keep the property nullz (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.not.include('nullz');
-      done();
-    });  
+  it('should delete the property undf', function (done) {
+    expect(obj).to.not.include.keys('undf')
+    done()
+  })
 
-    it('Should not keep the property undf (strict mode) (recursive)', function(done) {
-      expect(obj.childrenObj).to.not.include('undf');
-      done();
-    });
-  });
-});
+  it('should clean childrenArr from null and undf values', function (done) {
+    expect(obj).to.include.keys('childrenArr')
+    expect(obj.childrenArr).to.eql([
+      'str',
+      1,
+      true,
+      fn,
+      1337
+    ])
+    done()
+  })
+
+  describe('It should have cleaned recursively', function () {
+    it('childrenObj should exist', function (done) {
+      expect(obj).to.include.keys('childrenObj')
+      expect(obj.childrenObj).to.be.an('object')
+      done()
+    })
+
+    it('should keep the property string', function (done) {
+      expect(obj.childrenObj).to.include.keys('string')
+      expect(obj.childrenObj.string).to.equal('testing')
+      done()
+    })
+
+    it('should keep the property number', function (done) {
+      expect(obj.childrenObj).to.include.keys('number')
+      expect(obj.childrenObj.number).to.equal(1337)
+      done()
+    })
+
+    it('should keep the property float', function (done) {
+      expect(obj.childrenObj).to.include.keys('float')
+      expect(obj.childrenObj.float).to.equal(3.14159)
+      done()
+    })
+
+    it('should delete the property falsy', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('falsy')
+      done()
+    })
+
+    it('should delete the property bool', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('bool')
+      done()
+    })
+
+    it('should keep the property date', function (done) {
+      expect(obj.childrenObj).to.include.keys('date')
+      expect(obj.childrenObj.date).to.be.a('date')
+      done()
+    })
+
+    it('should keep the property fn', function (done) {
+      expect(obj.childrenObj).to.include.keys('fn')
+      expect(obj.childrenObj.fn).to.be.a('function')
+      done()
+    })
+
+    it('should delete the property nullz', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('nullz')
+      done()
+    })
+
+    it('should delete the property undf', function (done) {
+      expect(obj.childrenObj).to.not.include.keys('undf')
+      done()
+    })
+
+    it('should clean childrenArr from null and undf values', function (done) {
+      expect(obj.childrenObj).to.include.keys('childrenArr')
+      expect(obj.childrenObj.childrenArr).to.eql([
+        'str',
+        1,
+        true,
+        fn,
+        1337
+      ])
+      done()
+    })
+  })
+})
